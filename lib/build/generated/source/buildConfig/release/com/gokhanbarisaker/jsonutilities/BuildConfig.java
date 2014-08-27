f7b9f3c1f490cfdc6149c13f115aedaa20fa1157
Gökhan Barış Aker <gokhanbarisaker@gmail.com>
2014-08-27 11:34:40 +0300

    asdfadsfadsfdsfasfasf

---
+++ lib/build/generated/source/buildConfig/release/com/gokhanbarisaker/jsonutilities/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.gokhanbarisaker.jsonutilities;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "com.gokhanbarisaker.jsonutilities";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
